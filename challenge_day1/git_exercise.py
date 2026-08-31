96f4039f8dd1a3aebe52f5aedba2540532715e64
runret <yelyzavetalisova2@gmail.com>
2026-08-31 11:12:29 +0200

    Group exercise

---
+++ challenge_day1/git_exercise.py
@@ -0,0 +1,13 @@
+import zoe
+import katharina
+import minseo
+import yasmin
+import eli
+
+def main():
+    print("This is team Pink Aliens! We are:")
+    print(zoe.zoe())
+    print(katharina.katharina())
+    print(minseo.minseo())
+    yasmin.yasmin()
+    eli.eli()
