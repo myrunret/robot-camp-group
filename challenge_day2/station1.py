be455c549bc59a5f11b0208abcc99b4265d6d045
Yasmin Wassie <yasminwassie@gmail.com>
2026-09-01 15:01:15 +0200

    it works now

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -3,18 +3,13 @@
 
 
 def solution_station_1(n: int):
-    if n < 0:
-        raise ValueError("n must be a non-negative integer")
     if n == 0:
         return 0
     if n == 1:
         return 1
-    
-    a, b = 0, 1
-    for _ in range(2, n + 1):
-        a, b = b, a + b
-    return b
+    # Recursive case
+    return solution_station_1(n - 1) + solution_station_1(n - 2)
 
 
-print(solution_station_1(5))
-    
+print(solution_station_1(11))
+
