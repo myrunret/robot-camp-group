9f674571dd724dba9193e7b79da996207eac40db
runret <yelyzavetalisova2@gmail.com>
2026-09-01 14:42:32 +0200

    station 2 fix

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -2,6 +2,8 @@ import calendar
 
 def solution_station_2 (input):
     y,m,d = map(int, input.split('-'))
-    output = calendar.weekday(y,m,d)
+    weekday = calendar.weekday(y,m,d)
+    kanji = ['月','火','水','木','金','土','日']
+    output = kanji[weekday] + "曜日"
     return output
 
