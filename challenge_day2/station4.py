f53f61e7f9df29fee7baba6681ecdb5be6992b10
Zoë Barkhurst <zoebarkhurstl@u.boisestate.edu>
2026-09-01 15:02:28 +0200

    change

--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -1,9 +1,9 @@
-##solution station 4 
-## observations 76 false: 99 false 33 false 
+##solution station 4
+## observations 76 false: 99 false 33 false
+
 
 def station4(n: int):
     try:
-        
         if n <= 1:
             return False
         if n <= 3:
@@ -11,7 +11,7 @@ def station4(n: int):
         if n % 2 == 0 or n % 3 == 0:
             return False
         i = 5
-     while i * i <= n:
+        while i * i <= n:
             if n % i == 0 or n % (i + 2) == 0:
                 return False
             i += 6
