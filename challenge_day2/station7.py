fb0640a14869e4599e2a6d4acc27a04a1cc51925
alissamsh <alissa.mishima.uva@gmail.com>
2026-09-01 14:26:19 +0200

    update station 7

--- challenge_day2/station7.py
+++ challenge_day2/station7.py
@@ -1,2 +1,10 @@
-def solution_station_7():
-    return None
+def solution_station_7(expr: str):
+    values = {
+        "e": 0.5,
+        "c": 4,
+        "a": 3,
+        "b": -1,
+        "d": 7
+    }
+
+    return float(eval(expr, {}, values))
