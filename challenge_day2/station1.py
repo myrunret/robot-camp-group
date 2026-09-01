558434f740c055ed480147fa9f0720d7edf7c915
Yasmin Wassie <yasminwassie@gmail.com>
2026-09-01 14:48:07 +0200

    corrected station 1 solution due to exponential time complexity

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -3,13 +3,15 @@
 
 def solution_station_1(n):
     #base cases
-    if n==0:
-        return 0
-    if n == 1:
-        return 1
-    #recursive case
+    if n==0:return 0
+    if n == 1:return 1
 
-    return solution_station_1(n-1) + solution_station_1(n-2)
+    a,b = 0,1
+
+    for i in range(2, n+1):
+        a,b = b, a+b 
+        return b
+
 
 print(solution_station_1(53))
 print(solution_station_1(45))
